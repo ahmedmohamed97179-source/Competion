0f7cff115727b5f4c80bb3b9988dbf0857deefd1
Ahmed Mohamed <ahmedmohamed97179@gmail.com>
2026-05-30 16:33:32 +0300

    Update newbase.py

--- newbase.py
+++ newbase.py
@@ -1,1 +1,356 @@
 
+# Import modules
+import numpy as np  # Library for numerical computing and array operations
+import pandas as pd  # Library for handling tabular data
+import matplotlib.pyplot as plt  # Library for basic data visualization
+import seaborn as sns  # Library for statistical data visualization
+from sklearn.preprocessing import LabelEncoder  # Encoder for converting categorical variables to numeric labels
+from sklearn.ensemble import RandomForestClassifier  # Random Forest classifier
+from sklearn.model_selection import StratifiedKFold  # Class for stratified K-fold cross-validation
+from sklearn.metrics import roc_auc_score  # Metric function for computing ROC AUC
+from google.colab import drive
+drive.mount('/content/drive')
+
+from pathlib import Path
+
+PATH = Path.cwd() / "input"
+
+train_file = PATH / "train.csv"
+test_file = PATH / "test.csv"
+sample_sub_file = PATH / "sample_submission.csv"
+
+# Check if path exists
+if train_file.exists() and test_file.exists() and sample_sub_file.exists():
+    print("All files exist and path is correctly set.")
+else:
+    print("Some files are missing or path is not correctly set.")
+
+
+
+
+
+PATH = Path.cwd() / "input"
+
+train = pd.read_csv(PATH / "train.csv")
+test = pd.read_csv(PATH / "test.csv")
+sample_sub = pd.read_csv(PATH / "sample_submission.csv")
+
+print(f"train: {train.shape}, test: {test.shape}, sample_sub: {sample_sub.shape}")
+
+"""First, let's check the shape of the dataset."""
+
+print('Train:', train.shape)
+print('Test:', test.shape)
+
+
+
+drafted_counts = train['Drafted'].value_counts()
+
+plt.figure(figsize=(8, 6))
+plt.bar(drafted_counts.index.astype(str), drafted_counts.values)
+plt.title('Distribution of Drafted', fontsize=16)
+plt.xlabel('Drafted', fontsize=14)
+plt.ylabel('Count', fontsize=14)
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+
+plt.show()
+
+drafted_percentage = train['Drafted'].value_counts(normalize=True) * 100
+
+print(f"Percentage of 0: {drafted_percentage.get(0, 0):.2f}%")
+print(f"Percentage of 1: {drafted_percentage.get(1, 0):.2f}%")
+
+
+# Extraxt numeric columns (excluding the 'Id' and 'Drafted' columns)
+numeric_cols = train.select_dtypes(include=['number']).columns
+numeric_cols = numeric_cols.drop(['Id', 'Drafted'])
+
+# Plot
+num_cols = len(numeric_cols)
+cols = 3
+rows = (num_cols + cols - 1) // cols
+
+plt.figure(figsize=(5 * cols, 4 * rows))
+
+for i, col in enumerate(numeric_cols, 1):
+    plt.subplot(rows, cols, i)
+    plt.hist(train[col].dropna(), bins=30, edgecolor='black')
+    plt.title(f'Histogram of {col}')
+    plt.xlabel(col)
+    plt.ylabel('Frequency')
+
+plt.tight_layout()
+plt.show()
+
+"""Let's examine the correlation matrix of the numerical features using a heatmap."""
+
+# Extract numeric columns (excluding the 'Id' and ''Drafted' columns)
+numeric_cols = train.select_dtypes(include=['number']).drop(['Id', 'Drafted'], axis=1)
+
+# Compute the correlation matrix
+corr_matrix = numeric_cols.corr()
+
+# Plot a heatmap
+plt.figure(figsize=(12, 10))
+sns.heatmap(
+    corr_matrix,
+    annot=True,
+    fmt='.2f',
+    cmap='coolwarm',
+    vmin=-1, vmax=1,
+    square=True,
+    linewidths=0.5
+)
+
+plt.title('Correlation Matrix of Numeric Features', fontsize=16)
+plt.show()
+
+"""The correlation coefficient between **Agility_3cone** and **Shuttle** is **0.88**, indicating a strong positive relationship. These tests may rely on similar muscle groups and movement patterns.
+
+On the other hand, the correlation coefficient between **Sprint_40yd** and **Broad_Jump** is **-0.82**, showing a strong negative relationship. Since both events mainly depend on lower-body power, we might normally expect their scores to move together (i.e., a positive correlation), but here we observe the opposite. To investigate this further, let’s visualize **Sprint_40yd**.
+"""
+
+# Plot a boxplot
+plt.figure(figsize=(6, 3))
+sns.boxplot(x=train['Sprint_40yd'])
+
+plt.title('Box Plot of Sprint_40yd', fontsize=16)
+
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.show()
+
+
+
+# Extract categorical features
+categorical_cols = train.select_dtypes(include=['object', 'category']).columns
+
+# Get the number of unique categories in each column
+levels_count = {col: train[col].nunique() for col in categorical_cols}
+
+for col, count in levels_count.items():
+    print(f"{col}: {count} levels")
+
+"""**'School'** has **236** unique values, which is too many to visualize clearly, so we will exclude it from the plots here."""
+
+# Extract categorical features (object or category) and exclude the 'School' columns
+categorical_cols = train.select_dtypes(include=['object', 'category']).columns
+categorical_cols = categorical_cols.drop('School')
+
+# Prepare for plotting
+num_cols = len(categorical_cols)
+rows = 1
+cols = num_cols
+
+fig, axes = plt.subplots(rows, cols, figsize=(5 * cols, 5))
+
+if cols == 1:
+    axes = [axes]
+else:
+    axes = axes.flatten()
+
+
+# Count plots for each categorical features
+for i, col in enumerate(categorical_cols):
+    sns.countplot(x=col, data=train, order=train[col].value_counts().index, ax=axes[i])
+    axes[i].set_title(f'Count Plot of {col}', fontsize=14)
+    axes[i].set_xlabel(col, fontsize=12)
+    axes[i].set_ylabel('Count', fontsize=12)
+    axes[i].tick_params(axis='x', rotation=45)
+    axes[i].grid(axis='y', linestyle='--', alpha=0.7)
+
+plt.tight_layout()
+plt.show()
+
+
+# Extract categorical features (object or category) and exclude the 'School' column
+categorical_cols = train.select_dtypes(include=['object', 'category']).columns
+categorical_cols = categorical_cols.drop('School')
+
+# Prepare for plotting
+num_cols = len(categorical_cols)
+rows = 1
+cols = num_cols
+
+fig, axes = plt.subplots(rows, cols, figsize=(5 * cols, 5))
+
+if cols == 1:
+    axes = [axes]
+else:
+    axes = axes.flatten()
+
+# Plot the mean value of 'Drafted' for each categorical feature as bar charts
+for i, col in enumerate(categorical_cols):
+    mean_values = train.groupby(col)['Drafted'].mean().sort_values(ascending=False)
+    sns.barplot(x=mean_values.index, y=mean_values.values, ax=axes[i])
+    axes[i].set_title(f'Average Drafted by {col}', fontsize=14)
+    axes[i].set_xlabel(col, fontsize=12)
+    axes[i].set_ylabel('Average Drafted', fontsize=12)
+    axes[i].set_ylim(0, 1)
+    axes[i].tick_params(axis='x', rotation=45)
+    axes[i].grid(axis='y', linestyle='--', alpha=0.7)
+
+plt.tight_layout()
+plt.show()
+
+
+# Drop unused columns
+train = train.drop(columns=["Id", "School"])
+test = test.drop(columns=["Id","School"])
+
+"""### 4.2 Dealing Missing Values
+
+There are missing values in **Age**, **Sprint_40yd**, **Vertical_Jump**, **Bench_Press_Reps**, **Broad_Jump**, **Agility_3cone**, and **Shuttle**. In this notebook, we will impute these missing values using the mean of each column.
+"""
+
+# Columns to impute with the mean
+cols_to_fill = ['Age', 'Sprint_40yd', 'Vertical_Jump', 'Bench_Press_Reps',
+                'Broad_Jump', 'Agility_3cone', 'Shuttle']
+
+# Impute both train and test using the means computed from the training data
+for col in cols_to_fill:
+    mean_value = train[col].mean()
+    train[col] = train[col].fillna(mean_value)
+    test[col] = test[col].fillna(mean_value)
+
+"""Let's check whether there are any missing values."""
+
+train.isnull().sum()
+
+test.isnull().sum()
+
+
+# Label-encode the categorical features
+label_encoders = {}
+for c in ["Player_Type", "Position_Type", "Position"]:
+    label_encoders[c] = LabelEncoder()
+    train[c] = label_encoders[c].fit_transform(train[c].astype(str))
+    test[c] = label_encoders[c].transform(test[c].astype(str))
+
+
+# Split into features and target
+X = train.drop(columns=["Drafted"])
+y = train["Drafted"]
+
+# Set up the model and cross-validation
+model = RandomForestClassifier(
+    n_estimators=100,
+    max_depth=5,
+    random_state=2025
+)
+skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+
+# Store scores
+auc_scores = []
+
+# Train and evaluate using Startified K-Fold cross-validation
+for fold, (train_idx, valid_idx) in enumerate(skf.split(X, y)):
+    print(f"Fold {fold + 1}")
+
+    X_train, X_valid = X.iloc[train_idx], X.iloc[valid_idx]
+    y_train, y_valid = y.iloc[train_idx], y.iloc[valid_idx]
+
+    # Train the model
+    model.fit(X_train, y_train)
+
+    # Predict and score
+    y_valid_pred_proba = model.predict_proba(X_valid)[:, 1]
+    auc = roc_auc_score(y_valid, y_valid_pred_proba)
+    auc_scores.append(auc)
+    print(f"  AUC: {round(auc, 4)}")
+
+# Print mean AUC
+mean_auc = np.mean(auc_scores)
+print("\nAverage Validation AUC:", round(mean_auc, 4))
+
+
+for df in [train, test]:
+    df['BMI'] = df['Weight'] / (df['Height'] ** 2)
+
+"""Let's re-evaluate the model, this time including the newly created feature."""
+
+# Split into features and target
+X = train.drop(columns=["Drafted"])
+y = train["Drafted"]
+
+# Set up the model and cross-validation
+model = RandomForestClassifier(
+    n_estimators=100,
+    max_depth=5,
+    random_state=2025
+)
+skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+
+# Store scores
+auc_scores = []
+
+# Train and evaluate using Startified K-fold cross-validation
+for fold, (train_idx, valid_idx) in enumerate(skf.split(X, y)):
+    print(f"Fold {fold + 1}")
+
+    X_train, X_valid = X.iloc[train_idx], X.iloc[valid_idx]
+    y_train, y_valid = y.iloc[train_idx], y.iloc[valid_idx]
+
+    # Train the model
+    model.fit(X_train, y_train)
+
+    # Validation prediction & scoring
+    y_valid_pred_proba = model.predict_proba(X_valid)[:, 1]
+    auc = roc_auc_score(y_valid, y_valid_pred_proba)
+    auc_scores.append(auc)
+    print(f"  AUC: {round(auc, 4)}")
+
+# Print the mean AUC
+mean_auc = np.mean(auc_scores)
+print("\nAverage Validation AUC:", round(mean_auc, 4))
+
+
+# Create a DataFrame of features and their importances
+feature_importances = pd.DataFrame({
+    'Feature': X.columns,
+    'Importance': model.feature_importances_
+}).sort_values(by='Importance', ascending=False)
+
+# Visualize
+plt.figure(figsize=(10, 6))
+sns.barplot(data=feature_importances, x='Importance', y='Feature')
+plt.title('Feature Importances from Random Forest', fontsize=16)
+plt.xlabel('Importance')
+plt.ylabel('Feature')
+plt.tight_layout()
+plt.show()
+
+
+
+# Store predictions
+test_pred_proba_list = []
+
+# Train and evaluate using Startified K-fold cross-validation
+for fold, (train_idx, valid_idx) in enumerate(skf.split(X, y)):
+    X_train, X_valid = X.iloc[train_idx], X.iloc[valid_idx]
+    y_train, y_valid = y.iloc[train_idx], y.iloc[valid_idx]
+
+    # Train the model on this fold
+    model.fit(X_train, y_train)
+
+    # Save predictions for the test set
+    test_pred_proba = model.predict_proba(test)[:, 1]
+    test_pred_proba_list.append(test_pred_proba)
+
+# Average test predictions across folds
+test_pred_proba_mean = np.mean(test_pred_proba_list, axis=0)
+
+"""### 7.2 Saving the prediction as CSV file [DO NOT CHANGE]
+**WARNING**: DO **NOT** CHANGE THE CODES BELOW!!!
+"""
+
+# Running this cell will generate the submission CSV file
+submission = pd.read_csv(PATH / 'sample_submission.csv') # Update PATH in needed
+submission["Drafted"] = test_pred_proba_mean
+submission.to_csv(PATH / 'submission.csv', index=False)
+
+
+
+
+
+
+
